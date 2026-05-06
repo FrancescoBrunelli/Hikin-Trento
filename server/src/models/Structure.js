68f782f434bbe6c527e55e586cd25772a1a4f075
giulzzz04 <giuliacristofolini13@gmail.com>
2026-05-06 09:59:39 +0200

    created structure schema

--- server/src/models/Structure.js
+++ server/src/models/Structure.js
@@ -1,6 +1,21 @@
 const mongoose = require ('mongoose');
 const bcrypt = require ('bcrypt');
+const coordinatesSchema = require ('./schemas/coordinatesSchema');
 
 const structureSchema = new mongoose.Schema({
-    
+    name: {type: String, required: true},
+    name_owner: {type: String, required: true},
+    surname_owner: {type: String, required: true},
+    coordinates: {type: coordinatesSchema, required: true}, 
+    telephone: {type: Number, required: true},
+    password: {type: String, required: true}
+}, {timestamps: true});
+
+
+structureSchema.pre('save', async function(){
+    if (!this.isModified('password')) return;
+    this.password = await bcrypt.hash(this.password, 10);
 })
+
+
+module.exports = mongoose.model('Structure', structureSchema);
--- server/src/models/schemas/coordinatesSchema.js
+++ server/src/models/schemas/coordinatesSchema.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
 
 const coordinatesSchema = new mongoose.Schema({
-  latitude:  { type: Number, required: true },
-  longitude: { type: Number, required: true }
+  latitude:  {type: Number, required: true },
+  longitude: {type: Number, required: true },
+  altitude:  {type: Number, required: true }
 });
 
-module.exports = mongoose.model('Coordinate', coordinatesSchema);
+module.exports = coordinateSchema; 
