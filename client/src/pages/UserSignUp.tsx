164cb90d02b3abd2236c8d6cc0f8e598707c2d72
Lori Daniel <danielfernando.lori@studenti.unitn.it>
2026-05-14 09:56:33 +0200

    fix: fixed ports in SignIn and SignUp

--- client/src/pages/UserSignUp.tsx
+++ client/src/pages/UserSignUp.tsx
@@ -21,7 +21,7 @@ function UserSignUp() {
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/auth/register', {
+            const response = await fetch('http://localhost:3000/api/auth/register', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
